e14271ed15709d8318851a066abfbea671dfe304
Max Pasichnyk <maximmkaaa@gmail.com>
2018-06-10 09:14:56 +0000

    Update lisp.cpp

--- lisp.cpp
+++ lisp.cpp
@@ -1,48 +1,49 @@
 #include "lisp.h"
+#include <sstream>
 
-static const unsigned char attr[256] = {
-    0,
-    1, 1, 1, 1, 1, 1, 1, 1, 2, 2,
-    2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 2, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 3, 3, 3,
-    3, 3, 3, 3, 3, 3, 3, 5, 5, 5,
-    5, 5, 5, 5, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    5, 5, 5, 5, 5, 5, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 5, 5, 5, 5, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1
-};
-
-static const unsigned char accept[6] = {
-    0,
-    0, 0, 3, 4, 5
-};
-
-static const char next[6][6] = {
-     0,  0,  0,  0,  0,  0,
-     0,  1,  2,  3,  4,  5,
-    -2, -2,  2, -2, -2, -2,
-    -3, -3, -3,  3, -3, -3,
-    -4, -4, -4, -4,  4, -4,
-    -5, -5, -5, -5, -5, -5
-};
+//static const unsigned char attr[256] = {
+//    0,
+//    1, 1, 1, 1, 1, 1, 1, 1, 2, 2,
+//    2, 2, 2, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 2, 5, 5, 5, 5, 5, 5, 5, 5,
+//    5, 5, 5, 5, 5, 5, 5, 3, 3, 3,
+//    3, 3, 3, 3, 3, 3, 3, 5, 5, 5,
+//    5, 5, 5, 5, 4, 4, 4, 4, 4, 4,
+//    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+//    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+//    5, 5, 5, 5, 5, 5, 4, 4, 4, 4,
+//    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+//    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+//    4, 4, 5, 5, 5, 5, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1
+//};
+//
+//static const unsigned char accept[6] = {
+//    0,
+//    0, 0, 3, 4, 5
+//};
+//
+//static const char next[6][6] = {
+//     0,  0,  0,  0,  0,  0,
+//     0,  1,  2,  3,  4,  5,
+//    -2, -2,  2, -2, -2, -2,
+//    -3, -3, -3,  3, -3, -3,
+//    -4, -4, -4, -4,  4, -4,
+//    -5, -5, -5, -5, -5, -5
+//};
 
 struct token {
     int type;
@@ -52,46 +53,67 @@ struct token {
 static std::vector<token> lex(const char* input) {
     std::vector<token> out;
 
+//    while (*input) {
+//        int state = 1;
+//
+//        const char* buf = input;
+//        while ((state = next[state][attr[*input]]) > 0) {
+//            input++;
+//        }
+//        state = -state;
+//
+//        switch (accept[state]) {
+//            case 3: out.push_back({256, std::string(buf, input - buf)}); break;
+//            case 4: out.push_back({257, std::string(buf, input - buf)}); break;
+//            case 5: out.push_back({buf[0], std::string(buf, input - buf)}); break;
+//            default: break;
+//        }
+//    }
+
     while (*input) {
-        int state = 1;
+        while (isspace(*input)) ++input;
 
-        const char* buf = input;
-        while ((state = next[state][attr[*input]]) > 0) {
+        if (input[0] == '(') {
+            out.push_back({'(', "("});
             input++;
-        }
-        state = -state;
-
-        switch (accept[state]) {
-            case 3: out.push_back({256, std::string(buf, input - buf)}); break;
-            case 4: out.push_back({257, std::string(buf, input - buf)}); break;
-            case 5: out.push_back({buf[0], std::string(buf, input - buf)}); break;
-            default: break;
-        }
+        } else if (input[0] == ')') {
+            out.push_back({')', ")"});
+            input++;
+        } else if (isalpha(input[0]) || (input[0] == '_')) {
+            const char* buf = input++;
+            while (isalnum(input[0]) || (input[0] == '_')) ++input;
+            out.push_back({257, std::string(buf, input - buf)});
+        } else if (isdigit(input[0]) || (input[0] == '-') && isdigit(input[1])) {
+            const char* buf = input++;
+            while (isdigit(input[0])) ++input;
+            if (input[0] == '.') ++input;
+            while (isdigit(input[0])) ++input;
+            out.push_back({256, std::string(buf, input - buf)});
+        } else if (ispunct(input[0])) {
+            const char* buf = input++;
+            while (ispunct(input[0]) && (input[0] != '(') && (input[0] != ')')) {
+                ++input;
+            }
+            out.push_back({257, std::string(buf, input - buf)});
+        } else ++input;
     }
 
     return out;
 }
 
-lisp::Cell::Cell(Cell::Type type) : type(type) {}
+lisp::Cell::Cell(Cell::Type type) : type(Pointer), ptr(0) {}
+lisp::Cell::Cell(void* ptr) : type(Pointer), ptr(ptr) {}
 lisp::Cell::Cell(int number) : type(Number), number(number) {}
 lisp::Cell::Cell(std::string symbol) : type(Symbol), symbol(std::move(symbol)) {}
 lisp::Cell::Cell(std::vector<Cell> list) : type(List), list(std::move(list)) {}
 lisp::Cell::Cell(Cell(*proc)(Env* , Args)) : type(Proc), proc(proc) {}
 
-lisp::Cell lisp::Cell::operator()(lisp::Env* env, lisp::Args args) {
-    if (type == lisp::Cell::Proc) {
-        return proc(env, args);
-    }
-    if (type == lisp::Cell::Lambda) {
-        auto context = new Env(list[1].list, args, env);
-        auto result = eval(context, list[2]);
-        delete context;
-        return result;
-    }
-    return 0;
-}
-
 std::string lisp::Cell::to_string() const {
+    if (type == Type::Pointer) {
+        std::stringstream stream;
+        stream << ptr;
+        return stream.str();
+    }
     if (type == Type::Symbol) return symbol;
     if (type == Type::Number) return std::to_string(number);
     if (type == Type::List) {
@@ -167,10 +189,28 @@ lisp::Cell lisp::eval(Env* env, Cell cell) {
             cell.type = Cell::Lambda;
             return cell;
         }
+        if (cell.list[0].symbol == "begin") {
+            auto context = new Env({}, {}, env);
+            for (size_t i = 1; i < cell.list.size() - 1; ++i) {
+                eval(env, cell.list[i]);
+            }
+            auto result = eval(env, cell.list[cell.list.size() - 1]);
+            delete context;
+            return result;
+        }
 
         for (auto& arg : cell.list) arg = eval(env, arg);
 
-        return cell.list[0](env, std::vector<Cell>(cell.list.begin() + 1, cell.list.end()));
+        if (cell.list[0].type == Cell::Proc) {
+            return cell.list[0].proc(env, std::vector<Cell>(cell.list.begin() + 1, cell.list.end()));
+        }
+
+        if (cell.list[0].type == Cell::Lambda) {
+            auto context = new Env(cell.list[0].list[1].list, std::vector<Cell>(cell.list.begin() + 1, cell.list.end()), env);
+            auto result = eval(context, cell.list[0].list[2]);
+            delete context;
+            return result;
+        }
     }
     return cell;
 }
